960f398568cc6161cbdfe97ec257243ec0e6046e
qq.bareta <qq.bareta@gmail.com>
2012-11-16 13:11:26 +0000

    koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,46 @@
+<?php
+
+class db{
+
+	private $server;
+	private $user;
+	private $pass;
+	private $db;
+	private $conn;
+		
+	public function __construct($server="localhost", $user="root", $pass=""){
+		$this->server=$server;
+		$this->user=$user;
+		$this->password=$pass;
+	}
+
+	public function konek(){
+		$this->conn=mysql_connect($this->server,$this->user,$this->pass);
+		if(!$this->conn){
+			echo "koneksi server error"."\n";
+			throw new MySQLException(mysql_error(), mysql_errno());
+		} 
+		//else {
+			//echo "koneksi server berhasil";
+		//}
+	}
+	
+	public function konekDb($db="bubuku"){
+		$this->db=mysql_select_db($db,$this->conn);
+		if(!$this->db){
+			echo "koneksi database error"."\n";
+			throw new MySQLException(mysql_error(), mysql_errno());
+		} 
+		//else {
+			//echo "koneksi db berhasil";
+		//}
+	}
+
+}
+
+
+
+
+
+
+
