56a3d6cc9fc967c31a3bb3ba59b328a5cf56d7aa
tiffanyrachim18@gmail.com <tiffanyrachim18@gmail.com@9d71241e-af6e-3882-4183-7f02aef0ae63>
2012-12-21 07:17:34 +0000

    halaman wishlist issues no 12

---
+++ trunk/website design/halaman wishlist.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+
+    <link href="bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+    </style>
+    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+
+        <div class="container-fluid">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">Bubuku</a>
+          <div class="nav-collapse collapse">
+
+            <p class="navbar-text pull-right">
+            
+       
+            </p>
+            <ul class="nav">
+              <li class="active"><a href="#"></a></li>
+              <li><a href="#about">Beranda</a></li>
+              <li><a href="#contact">Produk Kami</a></li>
+
+              <li><a href="#contact">Cara Pemesanan</a></li>
+              <li><a href="#contact">Tentang Kami</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <div class="span3">
+          <div class="well sidebar-nav">
+            <ul class="nav nav-list">
+              <li class="nav-header">Kategori</li>
+              
+        <li class="active"><a href="#">Agama & filsafat</a></li>
+
+        <li class="active"><a href="#">Bahasa</a></li>
+        <li class="active"><a href="#">Buku Anak &amp; Remaja</a></li>
+        <li class="active"><a href="#">Buku Impor</a></li>
+        <li class="active"><a href="#">Buku Sekolah</a></li>
+        <li class="active"><a href="#">Ekonomi</a></li>
+
+        <li class="active"><a href="#">Hobi &amp; Interest</a></li>
+        <li class="active"><a href="#">Hukum</a></li>
+        <li class="active"><a href="#">Kedokteran</a></li>
+        <li class="active"><a href="#">Kesehatan</a></li>
+        <li class="active"><a href="#">Kesekretariatan</a></li>
+
+        <li class="active"><a href="#">Wanita</a></li>
+        <li class="active"><a href="#">Komputer</a></li>
+        <li class="active"><a href="#">Majalah</a></li>
+        <li class="active"><a href="#">Manajemen &amp; Bisnis</a></li>
+        <li class="active"><a href="#">Pariwisata &amp; Peta</a></li>
+
+        <li class="active"><a href="#">Pengembangan Diri dan Karir</a></li>
+        <li class="active"><a href="#">Pertanian</a></li>
+        <li class="active"><a href="#">Psikologi &amp; Pendidikan</a></li>
+        <li class="active"><a href="#">Referensi &amp; Kamus</a></li>
+        <li class="active"><a href="#">Sastra &amp; Novel</a></li>
+
+        <li class="active"><a href="#">Sosial Politik</a></li>
+        <li class="active"><a href="#">Teknik</a></li>
+            </ul>
+          </div><!--/.well -->
+        </div><!--/span-->
+        <div class="span9">
+          <div class="hero-unit">
+          <center> <h1>WISHLIST</h1></center><br>
+           <form class="form-horizontal">
+  <div class="control-group">
+    <label class="control-label" for="inputJudulBuku">Judul Buku</label>
+    <div class="controls">
+      <input type="text" id="inputJudulBuku" placeholder="Judul Buku">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputTahunTerbit">Tahun Terbit</label>
+    <div class="controls">
+      <input type="text" id="inputTahunTerbit" placeholder="Tahun Terbit">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputPenulis">Penulis</label>
+    <div class="controls">
+      <input type="text" id="inputPenulis" placeholder="Penulis">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputGrade">Grade</label>
+    <div class="controls">
+      <input type="text" id="inputGrade" placeholder="Grade">
+    </div>
+  </div>
+  
+<div class="control-group">
+    <label class="control-label" for="inputBatasWaktu">Batas Waktu</label>
+    <div class="controls">
+      <input type="text" id="inputBatasWaktu" placeholder="Batas Waktu">
+    </div>
+  </div>
+ <center><button type="submit" class="btn">SUBMIT</button>
+ <button type="submit" class="btn">CANCEL</button>
+</form></center><br>
+          </div>
+
+         
+
+      <hr>
+
+      <footer>
+        
+      </footer>
+
+    </div><!--/.fluid-container-->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+
+    <script src="../assets/js/bootstrap-transition.js"></script>
+    <script src="../assets/js/bootstrap-alert.js"></script>
+    <script src="../assets/js/bootstrap-modal.js"></script>
+    <script src="../assets/js/bootstrap-dropdown.js"></script>
+    <script src="../assets/js/bootstrap-scrollspy.js"></script>
+    <script src="../assets/js/bootstrap-tab.js"></script>
+
+    <script src="../assets/js/bootstrap-tooltip.js"></script>
+    <script src="../assets/js/bootstrap-popover.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+    <script src="../assets/js/bootstrap-collapse.js"></script>
+    <script src="../assets/js/bootstrap-carousel.js"></script>
+    <script src="../assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
