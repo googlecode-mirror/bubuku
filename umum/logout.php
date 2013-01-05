01808acda4c61d08f9d5538bfb61fda42ecf974d
qq.bareta <qq.bareta@gmail.com>
2013-01-05 19:50:38 +0000

    modif logout (done) beserta destroy session untuk issues no 35

--- umum/logout.php
+++ umum/logout.php
@@ -1,3 +1,24 @@
+<?php
+include '../koneksi.php';
+include '../dao/auth.php';
+
+$koneksi=new koneksi();
+$koneksi->konek();
+$koneksi->konekDb();
+
+if(isset($_POST['logout']))
+{	
+	$auth=new auth();
+	$auth->logout();
+	header("Location: home_guest.php");	
+}
+
+if(isset($_POST['tidak']))
+{
+	header("Location: home_user.php");	
+}
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -14,8 +35,8 @@
     <p>Apakah anda yakin akan keluar?</p>
   </div>
   <div class="modal-footer">
-    <a href="home_guest.php"><button class="btn btn-primary">Iya</button></a>
-    <button class="btn" data-dismiss="modal" aria-hidden="true">Tidak</button>
+    <button class="btn btn-primary" name="logout">Iya</button>
+    <button class="btn" data-dismiss="modal" aria-hidden="true" name="tidak">Tidak</button>
   </div>
 </div>
       <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
@@ -27,7 +48,4 @@
   </div>
 </body>
 </html>
-<?php
-include "../dao/auth.php";
-$auth=new Auth();
-$auth->logout();
+
