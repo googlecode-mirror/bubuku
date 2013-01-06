6448c1f0f57788bf3612d621d6bcdd4937336c83
farrohmulyadi <farrohmulyadi@9d71241e-af6e-3882-4183-7f02aef0ae63>
2013-01-06 15:28:56 +0000

    1. issue 1 - login admin disambungkan dengan database 2. issue 34 - logout disambungkan dengan database 3. Issue 31 - manajemen user disambungkan dengan database 4. Issue 31 - manajemen edit user 5. Issue 26 - edit profil admin/A0_login_admin.php7/ 6. Desain home guest

--- trunk/umum/home_guest.php
+++ trunk/umum/home_guest.php
@@ -1,27 +1,20 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Home Guest</title>
-    <meta name="viewport" content="width=device-width, initial-scale=2.0">
-   
-     <!-- Le styles -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-         padding-bottom: 30px;
-      }
-    </style>
-    <link href="css/bootstrap-responsive.css" rel="stylesheet">
-<body background="img/blue.jpg">
-<div class="user">
-<h1><font color="#FF0000">BUBUKU</h1></div></font>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+<link href="css/bootstrap-responsive.css" rel="stylesheet" type="text/css" />
+<title></title>
+</head>
+<body background="img/white.jpg">
+<body>
+<div class="admin">
+  <h1><font color="#333333">BUBUKU</h1>
+</div>
 <!-- memuat menu -->
-   <div class="navbar navbar">
-    <div class="navbar-inner">
-    <a class="brand" href="#"></a>
+<div class="navbar navbar">
+<div class="navbar-inner">
+<a class="brand" href="#"></a>
     <ul class="nav">
 <li class="dropdown">
         <a class="dropdown-toggle" data-toggle="dropdown" href="home_user.php">Beranda
@@ -75,27 +68,25 @@
             <li><a href="cara_pemesanan.php">Cara Pemesanan</a></li>
             <li><a href="hubungi.php">Hubungi Kami</a></li>
         </ul>
-    </li>  
-           
-    <div class="span4 offset4" align="right">
-    <ul class="nav">
-    <li><a href="login_form_umum.php">Login</a></li>
-    <li><a href="registrasi.php">Daftar</a></li>          
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-      <script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
-    <!-- Bootstrap jQuery plugins compiled and minified -->
-    <script src="js/bootstrap.min.js"></script>
-</li>
-</ul>
-</div>
-</ul>
-</div>
-</div>
-<![endif]--></head>
-
-<body>
-
-<div class="container">
+    </li>    
+<!-- Menu Login dan Daftar -->
+<div class="offset8" align="right">
+<ul class="nav">
+<li><form method="POST" action="?menu=tcari"> 
+   <div>
+        <input type="text" name="kata" maxlength="50" value="Pencarian..." onBlur="if(this.value=='') this.value='Pencarian...';" onFocus="if(this.value=='Pencarian...') this.value='';" />
+        <input type="submit" value="Go" />
+      </div>
+    </form>
+    </li>
+<li class="button"> <a class="button" href="login_form_umum.php">Login</a> </li>
+<li class="button"> <a class="button" href="registrasi.php">Daftar</a> </li>
+  </ul>
+  </div>
+  </div>
+  </div>
+    <!-- awal form pencarian -->
+  <div class="container">
 <div class="header">
   <table align=center cellpadding=0 cellspacing=0 width=100%><tr><td align=center height=31>
 <table width=880><tr><td>
@@ -108,68 +99,183 @@ Kami berusaha untuk memberikan informasi dan pelayanan yang terbaik bagi anda! <
   <div class="sidebar1">
   <div class="intro">
     <p><font face=tahoma size=2><span id="date"></span> <span id="clock"></span></font></p>
-    <form method="POST" action="?menu=tcari"> 
-      <!-- form pencarian -->
-      <div>
-        <input type="text" name="kata" maxlength="50" value="Pencarian..." onBlur="if(this.value=='') this.value='Pencarian...';" onFocus="if(this.value=='Pencarian...') this.value='';" />
-        <input type="submit" value="Go" />
+     <!-- done form pencarian -->
+<!-- Isi dari Home Guest -->
+<div class="container-fluid" align="left">
+  <table width="1096" border="0" align="center">
+    <tr id="1">
+      <td width="275"><div class="dropdown-toggle">
+        <h2 align="left">Buku Per Kategori</h2>
+        <div>
+          <li><a href="#">Agama & Filsafat</a></li>
+          <li><a href="#">Bahasa</a></li>
+          <li><a href="#">Buku Anak & Remaja</a></li>
+          <li><a href="#">Buku Impor</a></li>
+          <li><a href="#">Buku Sekolah</a></li>
+          <li><a href="#">Ekonomi</a></li>
+          <li><a href="#">Hobi & Interest</a></li>
+          <li><a href="#">Hukum</a></li>
+          <li><a href="#">Kedokteran</a></li>
+          <li><a href="#">Kesehatan</a></li>
+          <li><a href="#">Kesekretariatan</a></li>
+          <li><a href="#">Wanita</a></li>
+          <li><a href="#">Komputer</a></li>
+          <li><a href="#">Majalah</a></li>
+          <li><a href="#">Manajemen & Bisnis</a></li>
+          <li><a href="#">Pariwisata & Peta</a></li>
+          <li><a href="#">Pengembangan Diri & Karir</a></li>
+          <li><a href="#">Pertanian</a></li>
+          <li><a href="#">Psikologi & Pendidikan</a></li>
+          <li><a href="#">Referensi & Kamus</a></li>
+          <li><a href="#">Sastra & Novel</a></li>
+          <li><a href="#">Sosial Politik</a></li>
+          <li><a href="#">Teknik</a></li>
+        </div></td>
+      <td><div>
+      <table width="545" border="0">
+      <tr>
+      <h2 align="center"><a>RECOMMENDED BOOK</a></h2>
+      </tr>
+      <tr>
+          <td><div class="img-circle">
+    <img src="img/home1.jpg" width="150" height="175"/></td>
+          <td><div><table width="300" border="0">
+          <tr><a>JUDUL BUKU </a></tr>
+          <td>Harga</td>
+          <td><input name="Harga" size="30"></td>
+        </tr>
+        <tr>
+          <td>Penerbit</td>
+          <td><input name="Penerbit" size="30"></td>
+        </tr>
+        </table>
+              <tr>
+          <td><div class="img-circle">
+    <img src="img/home1.jpg" width="150" height="175"/></td>
+          <td><div><table width="300" border="0">
+          <tr><a>JUDUL BUKU </a></tr>
+          <td>Harga</td>
+          <td><input name="Harga" size="30"></td>
+        </tr>
+        <tr>
+          <td>Penerbit</td>
+          <td><input name="Penerbit" size="30"></td>
+        </tr>
+      </table>
+      <tr>
+          <td><div class="img-circle">
+    <img src="img/home1.jpg" width="150" height="175"/></td>
+          <td><div><table width="300" border="0">
+          <tr><a>JUDUL BUKU </a></tr>
+          <td>Harga</td>
+          <td><input name="Harga" size="30"></td>
+        </tr>
+        <tr>
+          <td>Penerbit</td>
+          <td><input name="Penerbit" size="30"></td>
+        </tr>
+        </table>
+        <tr>
+          <td><div class="img-circle">
+    <img src="img/home1.jpg" width="150" height="175"/></td>
+          <td><div><table width="300" border="0">
+          <tr><a>JUDUL BUKU </a></tr>
+          <td>Harga</td>
+          <td><input name="Harga" size="30"></td>
+        </tr>
+        <tr>
+          <td>Penerbit</td>
+          <td><input name="Penerbit" size="30"></td>
+        </tr>
+        </table>
       </div>
-    </form>
-   	</div>
-    <div class="container-fluid">
-        <a href="#"><img src="img/logo.jpg" alt="aaa" name="Insert_logo" width="259x" height="90px" id="Insert_logo" style="background-color: #8090AB; display:block;" /></a> 
-
-        <div class="row-fluid">
-        <div class="span3">
-          <div class="well sidebar-nav">
-            <ul class="nav nav-list">
-<li class="nav-header">Kategori</li>
-              
-        <li class="active"><a href="#">Agama & filsafat</a></li>
-
-        <li class="active"><a href="#">Bahasa</a></li>
-        <li class="active"><a href="#">Buku Anak &amp; Remaja</a></li>
-        <li class="active"><a href="#">Buku Impor</a></li>
-        <li class="active"><a href="#">Buku Sekolah</a></li>
-        <li class="active"><a href="#">Ekonomi</a></li>
-
-        <li class="active"><a href="#">Hobi &amp; Interest</a></li>
-        <li class="active"><a href="#">Hukum</a></li>
-        <li class="active"><a href="#">Kedokteran</a></li>
-        <li class="active"><a href="#">Kesehatan</a></li>
-        <li class="active"><a href="#">Kesekretariatan</a></li>
-
-        <li class="active"><a href="#">Wanita</a></li>
-        <li class="active"><a href="#">Komputer</a></li>
-        <li class="active"><a href="#">Majalah</a></li>
-        <li class="active"><a href="#">Manajemen &amp; Bisnis</a></li>
-        <li class="active"><a href="#">Pariwisata &amp; Peta</a></li>
-
-        <li class="active"><a href="#">Pengembangan Diri dan Karir</a></li>
-        <li class="active"><a href="#">Pertanian</a></li>
-        <li class="active"><a href="#">Psikologi &amp; Pendidikan</a></li>
-        <li class="active"><a href="#">Referensi &amp; Kamus</a></li>
-        <li class="active"><a href="#">Sastra &amp; Novel</a></li>
-
-        <li class="active"><a href="#">Sosial Politik</a></li>
-        <li class="active"><a href="#">Teknik</a></li>
-	</div>
-    </div>
-    <!-- end .sidebar1 --></div>
-  <div class="content">
-
-    <!-- end .content --></div>
-  <div class="sidebar2">
-
-    <!-- end .sidebar2 --></div>
-  <div class="footer">
-    <table align=center cellpadding=0 cellspacing=0 width=100%><tr><td align=center height=31>
-<table width=880><tr><td>
-<marquee><font face=tahoma size=2 color=#FFFFFF><span class="style29">@Kelompok 5-RBPL A</span> 
-</marquee>
-</td></tr></table>
-</td></tr></table>
-    <!-- end .footer --></div>
-  <!-- end .container --></div>
+      </td>
+      </table>
+     
+      <td><div>
+        <table width="175" border="0">
+          <tr>
+          <td> <td id="tabelputus" width="350"><div align="center">
+            <iframe title="Shoutbox" width="200" height="200"
+              src="http://shoutbox.widget.me/window.html?uid=36rnn"
+              frameborder="0" scrolling="auto"></iframe>
+                      <script src="http://shoutbox.widget.me/v1.js"
+            type="text/javascript" xml:space="preserve"></script>
+            <br /></div>
+            <a
+            href="http://shoutbox.widget.me" title="Shoutbox Widget"></a>
+            <a href="http://shoutbox-tutorials.blogspot.com"
+            title="Shoutbox Tutorials"></a><br />
+            <!--  End --></td>
+        </tr>
+        <tr>
+          <td><div align="center">
+          </div></td>
+          <td><table>
+            <tr></tr>
+          </table></td></td>
+            <td><div align="center">
+                <p>
+                  <!--  Skype 'My status' button http://www.skype.com/go/skypebuttons -->
+                  <script
+            type="text/javascript"
+            src="http://download.skype.com/share/skypebuttons/js/skypeCheck.js"
+            xml:space="preserve"></script>
+                  <a
+            href="skype:farrohmulyadi?call"><img
+            src="http://mystatus.skype.com/balloon/farrohmulyadi"
+            style="border: none;" width="150" height="60" alt="My status" /></a> <br />
+                  <br />
+                  <script
+            charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"
+            type="text/javascript" xml:space="preserve"></script>
+                  <script
+            type="text/javascript" xml:space="preserve">//<![CDATA[
+new TWTR.Widget({
+  version: 2,
+  type: 'profile',
+  rpp: 4,
+  interval: 30000,
+  width: 100,
+  height: 100,
+  theme: {
+    shell: {
+      background: '#225710',
+      color: '#ffffff'
+    },
+    tweets: {
+      background: '#000000',
+      color: '#ffffff',
+      links: '#4aed05'
+    }
+  },
+  features: {
+    scrollbar: true,
+    loop: true,
+    live: true,
+    behavior: 'default'
+  }
+}).render().setUser('farrohmulyadi').start();
+//]]></script>
+                  &lt; <a href="http://en-gb.facebook.com/farrohmulyadi" target="_TOP"
+              style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif;     font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;"
+              title="Farroh S. Mulyadi">Farroh Mulyadi</a><br />
+                  <a
+              href="http://en-gb.facebook.com/farrohmulyadi" target="_TOP"
+              title="Farroh Mulyadi"> <img src="https://www.facebook.com/photo.php?fbid=4355193192723&set=a.1553772118947.81692.1074506229&type=1&theater"
+              style="border: 0px;" alt="" /></a><br />
+                  <a
+              href="http://en-gb.facebook.com/badges/" target="_TOP"
+              style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;"
+              title="Make your own badge!"></a></p>
+              </div></td>
+        </table>
+    </tr>
+  </table>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
+  <!-- Bootstrap jQuery plugins compiled and minified -->
+  <script src="js/bootstrap.min.js"></script>
+</div>
 </body>
 </html>
